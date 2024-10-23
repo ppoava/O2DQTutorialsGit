6ef732c06f7006ae71e8554e82f0083fd4de56e3
Paul Veen <paul.veen@yahoo.com>
2024-10-23 10:17:08 +0200

    Initial commit

---
+++ compareMuonGlobalMuon.C
@@ -0,0 +1,59 @@
+// compareMuonGlobalMuon.C
+// Created: 22 oct 2024
+// Paul VEEN (paveen)
+// Simple macro to compare results from the O2DQ tutorial
+
+#include <iostream>
+#include <string>
+#include "TFile.h"
+#include "TH1F.h"
+#include "THashList.h"
+#include "TList.h"
+
+void drawHist()
+ {
+
+    std::cout << "hoi" << std::endl;
+
+    THashList *listMuon, *listGlobalMuon, *listMuonPbPb;
+    TList *subListMuon, *subListGlobalMuon, *subListMuonPbPb;
+    
+    // Global muon pp
+    TFile* fGlobalMuon = new TFile("AnalysisResults_hyperloop_globalMuon.root", "READ");
+    listGlobalMuon = (THashList*)fGlobalMuon->Get("analysis-same-event-pairing/output");
+    subListGlobalMuon = (TList*)listGlobalMuon->FindObject("PairsMuonSEPM_matchedQualityCuts");
+
+    TH1F *hMuonMass, *hGlobalMuonMass, *hMuonPbPbMass;
+    hGlobalMuonMass = (TH1F*)subListGlobalMuon->FindObject("Mass");
+    hGlobalMuonMass->Scale(1/(hGlobalMuonMass->Integral()));
+
+    // Standalone muon pp
+    TFile* fMuon = new TFile("AnalysisResults_hyperloop_muon.root", "READ");
+    listMuon = (THashList*)fMuon->Get("analysis-same-event-pairing/output");
+    subListMuon = (TList*)listMuon->FindObject("PairsMuonSEPM_muonQualityCuts");
+
+    hMuonMass = (TH1F*)subListMuon->FindObject("Mass");
+    hMuonMass->Scale(1/(hMuonMass->Integral()));
+
+   // Standalone muon PbPb
+   TFile* fMuonPbPb = new TFile("AnalysisResults_hyperloop_PbPbmuon.root", "READ");
+   listMuonPbPb = (THashList*)fMuonPbPb->Get("analysis-same-event-pairing/output");
+   subListMuonPbPb = (TList*)listMuonPbPb->FindObject("PairsMuonSEPM_muonQualityCuts");
+
+   hMuonPbPbMass = (TH1F*)subListMuonPbPb->FindObject("Mass");
+   hMuonPbPbMass->Scale(1/(hMuonPbPbMass->Integral()));
+
+   // Draw histograms
+   hGlobalMuonMass->Draw("hist E");
+   hMuonMass->SetLineColor(kRed);
+   hMuonMass->Draw("same hist E");
+   hMuonPbPbMass->SetLineColor(kBlack);
+   hMuonPbPbMass->Draw("hist E");
+  
+ } // drawHist()
+
+int compareMuonGlobalMuon() 
+ {
+    drawHist();
+    return 0;
+ }
